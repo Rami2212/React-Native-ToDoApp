b46210909c26ed316e447cd11b8be4af015d70ff
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-04-19 20:11:52 +0530

    add global styles

---
+++ src/constants/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from './colors';
+
+export const GLOBAL_STYLES = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BACKGROUND,
+    paddingHorizontal: 22,
+    paddingVertical: 23,
+  },
+});
